560467ffd060b6f6673be7c3545e71892d357bb3
Ronny Wang <ronnywang@gmail.com>
2016-08-29 23:46:27 +0800

    改從 middle2 API 驗證 secret

--- index.php
+++ index.php
@@ -4,9 +4,21 @@ $uri = $_SERVER['REQUEST_URI'];
 list(, $index, $other) = explode('/', $uri, 3);
 list($index, $secret) = explode(':', urldecode($index), 2);
 
-if ($secret != crc32($index . getenv('SECRET'))) {
-    echo 'wrong url';
-    exit;
+if (file_exists("/tmp/cache-{$index}-{$secret}")) {
+    // 如果有檔案就免查詢了
+} else {
+    $content = file_get_contents('https://middle2.com/api/checkelastic?index=' . urlencode($index) . '&secret=' . urlencode($secret) . '&sig=' . crc32($index . $secret . getenv('SECRET')));
+    if (!$obj = json_decode($content)) {
+        echo 'check api error';
+        exit;
+    }
+
+    if ($obj->error !== false) {
+        echo 'api error';
+        exit;
+    }
+
+    touch("/tmp/cache-{$index}-{$secret}");
 }
 
 $url = 'http://' . getenv('ESHOST') . "/{$index}/{$other}";
